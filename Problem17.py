5afc8aa1ecc3c038bacbfebe99ffd2fb805fb6d5
TannerDowhy <tad638@mail.usask.ca>
2020-09-06 01:53:27 -0600

    Problems 11-16

---
+++ Problem17.py
@@ -0,0 +1,9 @@
+singles = ["one","two","three","four","five","six", "seven", "eight", "nine", "ten"]
+words   = ["and", "teen", "hundred", "thousand"]
+tens    = ["twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+weirds  = ["eleven", "twelve", "thirteen"]
+
+count = 0
+for i in range(1,10000):
+	print(i)
+
